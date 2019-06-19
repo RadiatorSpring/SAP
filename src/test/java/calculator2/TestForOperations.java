1929b182aa4881889ca3aa1821a2e864e1b9e4c3
I516445 <alexggeorgiev@abv.bg>
2019-06-19 14:12:00 +0300

    some more changes

---
+++ src/test/java/calculator2/TestForOperations.java
@@ -0,0 +1,34 @@
+package calculator2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestForOperations {
+	@Test
+	public void testCalculatorFindingOperationsShouldReturnMinus() {
+		String testString = "1-2";
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(calculator.findOperations(testString).get(0),"-");
+	}
+	@Test
+	public void testCalculatorFindingOperationsShouldReturnMultiplication() {
+		String testString = "1100*2";
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(calculator.findOperations(testString).get(0),"*");
+	}
+	@Test
+	public void testCalculatorFindingOperationsShouldReturnDivision() {
+		String testString = "1100/2";
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(calculator.findOperations(testString).get(0),"/");
+	}
+	@Test
+	public void testCalculatorFindingOperationsShouldReturnAdding() {
+		String testString = "1100+2";
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(calculator.findOperations(testString).get(0),"/");
+	}
+}
