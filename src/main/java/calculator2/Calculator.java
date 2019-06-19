99355bedc023f7ea97ddeca928c5dbe06f106b9b
I516445 <I516445@SOFN60285621A.emea.global.corp.sap>
2019-06-19 10:28:18 +0300

    It even works with spaces

---
+++ src/main/java/calculator2/Calculator.java
@@ -0,0 +1,87 @@
+package calculator2;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+public class Calculator {
+
+	
+
+	protected List<String> findOperations(String text) {
+		Pattern patternForOperations = Pattern.compile("[+/*-]");
+		Matcher matcherForOperations = patternForOperations.matcher(text);
+		List<String> operations = new ArrayList<String>();
+
+		while (matcherForOperations.find()) {
+			String operation = matcherForOperations.group();
+			operations.add(operation);
+		}
+		
+		return operations;
+	}
+
+	protected List<Double> findArguments(String text) {
+
+		Pattern patternForNumbers = Pattern.compile("\\d+");
+		Matcher matcherForNumbers = patternForNumbers.matcher(text);
+		List<Double> listForArguments = new ArrayList<Double>();
+
+		while (matcherForNumbers.find()) {
+			listForArguments.add(Double.parseDouble(matcherForNumbers.group()));
+		}
+
+		return listForArguments;
+	}
+
+	protected double compute(List<String> operations, List<Double> arguments) {
+		double result = 0;
+		int i = 0;
+		if (operations.get(i).equals("+")) {
+			result = arguments.get(i) + arguments.get(i + 1);
+		} else if (operations.get(i).equals("*")) {
+			result = arguments.get(i) * arguments.get(i + 1);
+		} else if (operations.get(i).equals("/")) {
+			result = arguments.get(i) / arguments.get(i + 1);
+		} else if (operations.get(i).equals("-")) {
+			result = arguments.get(i) - arguments.get(i + 1);
+		}
+		for (int j = 2; j < arguments.size(); j++) {
+			if(operations.get(j-1).equals("+")) {
+				result+= arguments.get(j);
+			}
+			else if(operations.get(j-1).equals("*")) {
+				result*= arguments.get(j);
+			}
+			else if(operations.get(j-1).equals("/")) {
+				result/= arguments.get(j);
+			}
+			else if(operations.get(j-1).equals("-")) {
+				result-= arguments.get(j);
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
